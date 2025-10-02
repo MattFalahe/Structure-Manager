bb31cd4b660266beacde11aaaa36ddca0b988fce
MattFalahe <47831385+MattFalahe@users.noreply.github.com>
2025-10-02 06:28:56 +0100

    Create StructureFuelHistory.php

---
+++ src/Models/StructureFuelHistory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace StructureManager\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StructureFuelHistory extends Model
+{
+    protected $table = 'structure_fuel_history';
+    
+    protected $fillable = [
+        'structure_id',
+        'corporation_id',
+        'fuel_expires',
+        'days_remaining',
+        'fuel_blocks_used',
+        'daily_consumption',
+    ];
+    
+    protected $dates = [
+        'fuel_expires',
+        'created_at',
+        'updated_at',
+    ];
+    
+    public function structure()
+    {
+        return $this->belongsTo(\Seat\Eveapi\Models\Corporation\CorporationStructure::class, 'structure_id', 'structure_id');
+    }
+}
