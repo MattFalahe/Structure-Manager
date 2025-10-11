5ed2df59008f8e9ab1252a095e02b6c1f5b8dd71
MattFalahe <47831385+MattFalahe@users.noreply.github.com>
2025-10-11 02:52:35 +0100

    Update StructureFuelHistory.php

--- src/Models/StructureFuelHistory.php
+++ src/Models/StructureFuelHistory.php
@@ -18,10 +18,14 @@ class StructureFuelHistory extends Model
         'consumption_rate',
         'tracking_type',
         'metadata',
+        'magmatic_gas_quantity',  // NEW: v1.0.6 - Metenox support
+        'magmatic_gas_days',       // NEW: v1.0.6 - Metenox support
     ];
     
     protected $casts = [
         'metadata' => 'array',
+        'magmatic_gas_quantity' => 'integer',
+        'magmatic_gas_days' => 'float',
     ];
     
     protected $dates = [
@@ -33,5 +37,42 @@ class StructureFuelHistory extends Model
     public function structure()
     {
         return $this->belongsTo(\Seat\Eveapi\Models\Corporation\CorporationStructure::class, 'structure_id', 'structure_id');
+    }
+    
+    /**
+     * Check if this record is for a Metenox Moon Drill
+     */
+    public function isMetenox()
+    {
+        if ($this->metadata && isset($this->metadata['is_metenox'])) {
+            return $this->metadata['is_metenox'] === true;
+        }
+        return false;
+    }
+    
+    /**
+     * Get the limiting factor for Metenox (fuel_blocks or magmatic_gas)
+     */
+    public function getMetenoxLimitingFactor()
+    {
+        if (!$this->isMetenox()) {
+            return null;
+        }
+        
+        if ($this->metadata && isset($this->metadata['limiting_factor'])) {
+            return $this->metadata['limiting_factor'];
+        }
+        
+        return null;
+    }
+    
+    /**
+     * Check if Metenox has critical magmatic gas levels (< 7 days)
+     */
+    public function hasLowMagmaticGas()
+    {
+        return $this->isMetenox() && 
+               $this->magmatic_gas_days !== null && 
+               $this->magmatic_gas_days < 7;
     }
 }
